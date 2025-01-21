3615dd6f8c3488f42c97acf8cdd11915fbb9225f
RenV564 <renatavero564@gmail.com>
2025-01-21 16:20:17 +0700

    second commit

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GUNTHA-STORE</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    </head>
+<body>
+<h1>GUNTHA-STORE</h1>
+<div class="container">
+    <br>
+    @yield('content')
+</div>
+
+</body>
+</html>
